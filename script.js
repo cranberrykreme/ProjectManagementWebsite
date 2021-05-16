e1e1dc57fb6f71169799f26e3d60ad8853489b76
Christopher Purkiss <christopher.purkiss@students.mq.edu.au>
2021-05-16 22:14:45 +1000

    Adding more JavaScript, can change job column and delete jobs

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let counter = 0;
+let id = 0;
+console.log("hello");
+
 // Selectors
 const jobInput = document.querySelector('.input-job');
 const jobButton = document.querySelector('.job-button');
 const jobList = document.querySelector('.job-list');
+const specificJob = document.querySelector('.job-list');
 
 
 // Event Listeners
@@ -10,23 +15,76 @@ jobButton.addEventListener('click', addJob);
 // Functions
 function addJob(event){
     event.preventDefault();
+    if(jobInput.value !== ""){
+        // Create labels in the DOM
+        const jobDiv = document.createElement('div');
+        jobDiv.classList.add('job');
+        //jobDiv.setAttribute("ondrop", "drop(event)")
+        counter++;
+        jobDiv.setAttribute("id", "div" + counter);
+        jobDiv.setAttribute("draggable", "true");
+        jobDiv.setAttribute("ondragstart","onDragStart(event);");
+        //jobDiv.setAttribute("ondrop","dropItem(event);");
+        //jobDiv.setAttribute("mousedown", "getId(this.id)");
+        //jobDiv.setAttribute("ondragover", "allowDrop(event)")
+
+        // Create job
+        const newJob = document.createElement('li');
+        newJob.innerHTML = jobInput.value;
+        newJob.classList.add('job-item');
+        //newJob.setAttribute("draggable","true");
+        //newJob.setAttribute("ondragstart","drag(event)");
+        newJob.setAttribute("id","drag" + counter);
+        jobDiv.appendChild(newJob);
+        console.log(jobInput.value);
+        console.log(jobDiv);
+        console.log(jobList);
+
+
+        jobList.appendChild(jobDiv);
+
+        jobInput.value = "";
+    }
 
     //console.log('hello');
+}
 
-    // Create labels in the DOM
-    const jobDiv = document.createElement('div');
-    jobDiv.classList.add('job');
+// ---------------------------- Section for drag and drop ---------------------------------
 
-    // Create job
-    const newJob = document.createElement('li');
-    newJob.innerHTML = jobInput.value;
-    newJob.classList.add('job-item');
-    jobDiv.appendChild(newJob);
-    console.log(jobInput.value);
-    console.log(jobDiv);
-    console.log(jobList);
+function onDragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    console.log(event.currentTarget);
+    //event.currentTarget.style.backgroundColor = 'red';
+}
 
-    jobList.appendChild(jobDiv);
+function onDragOver(event) {
+    event.preventDefault();
 
-    jobInput.value = "";
+}
+
+function dropItem(event){
+    console.log("this is called");
+    event.currentTarget.style.backgroundColor = 'white';
+}
+
+function onDrop(event) {
+    const id = event.dataTransfer.getData('Text');
+
+    const draggableElement = document.getElementById(id);
+
+    const dropzone = event.target;
+
+    console.log("The target is: " + event.target);
+
+    dropzone.appendChild(draggableElement);
+
+    event.dataTransfer.clearData();
+
+
+}
+
+function dropDelete(event) {
+    const ID = event.dataTransfer.getData('Text');
+    const myObj = document.getElementById(ID);
+    myObj.remove();
 }
